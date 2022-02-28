0bde44f335ce248dacee8952f73c03353163756f
Batmunkh Namshir <namshir17@gmail.com>
2022-02-28 15:59:45 -0800

    commit tree d27c5955a1e3c8554ead993498e9eedd278ae6ea

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,11 +6,19 @@ class Person
   end
 
   def get_married(person)
+    if person.is_a?(Person)
     self.partner = person
     person.partner = self
+    else
+      raise PartnerError
   end
 
 end
+
+ class PartnerError < StandardError
+  end
+end
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
